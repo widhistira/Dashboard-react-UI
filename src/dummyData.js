a5f75a5c0944a388bc739e2092150afb94ab931f
Eka Rudianto <eka.rudianto@thoughtworks.com>
2021-12-18 21:48:41 +0700

    Perubahan struktur

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,151 +1,183 @@
 export const userData = [
-    {
-      name: "Jan 2021",
-      "Active User": 1550,
-    },
-    {
-      name: "Feb 2021",
-      "Active User": 2250,
-    },
-    {
-      name: "Mar 2021",
-      "Active User": 3630,
-    },
-    {
-      name: "Apr 2021",
-      "Active User": 6000,
-    },
-    {
-      name: "May 2021",
-      "Active User": 4600,
-    },
-    {
-      name: "Jun 2021",
-      "Active User": 7000,
-    },
-    {
-      name: "Jul 2021",
-      "Active User": 3050,
-    },
-    {
-      name: "Agt 2021",
-      "Active User": 4000,
-    },
-    {
-      name: "Sep 2021",
-      "Active User": 7000,
-    },
-    {
-      name: "Oct 2021",
-      "Active User": 6000,
-    },
-    {
-      name: "Nov 2021",
-      "Active User": 7600,
-    },
-    {
-      name: "Dec 2021",
-      "Active User": 8500,
-    },
-  ];
+  {
+    name: "Jan 2021",
+    "Active User": 1550,
+  },
+  {
+    name: "Feb 2021",
+    "Active User": 2250,
+  },
+  {
+    name: "Mar 2021",
+    "Active User": 3630,
+  },
+  {
+    name: "Apr 2021",
+    "Active User": 6000,
+  },
+  {
+    name: "May 2021",
+    "Active User": 4600,
+  },
+  {
+    name: "Jun 2021",
+    "Active User": 7000,
+  },
+  {
+    name: "Jul 2021",
+    "Active User": 3050,
+  },
+  {
+    name: "Agt 2021",
+    "Active User": 4000,
+  },
+  {
+    name: "Sep 2021",
+    "Active User": 7000,
+  },
+  {
+    name: "Oct 2021",
+    "Active User": 6000,
+  },
+  {
+    name: "Nov 2021",
+    "Active User": 7600,
+  },
+  {
+    name: "Dec 2021",
+    "Active User": 8500,
+  },
+];
 
-  export const productData = [
-    {
-      name: "Okt 2021",
-      "Sales": 3500,
-    },
-    {
-      name: "Nov 2021",
-      "Sales": 2200,
-    },
-    {
-      name: "Des 2021",
-      "Sales": 5500,
-    },
-  ];
+export const productData = [
+  {
+    name: "Okt 2021",
+    "Sales": 3500,
+  },
+  {
+    name: "Nov 2021",
+    "Sales": 2200,
+  },
+  {
+    name: "Des 2021",
+    "Sales": 5500,
+  },
+];
 
-  export const userRows = [
-    {
-      id: 1,
-      username: "Ryan W",
-      email: "ryan@gmail.com",
-      status: "active",
-      transaction: "Rp.5.050.000",
-    },
-    {
-      id: 2,
-      username: "Andre",
-      email: "andre@gmail.com",
-      status: "active",
-      transaction: "Rp.1.550.000",
-    },
-    {
-      id: 3,
-      username: "Doni",
-      email: "doni@gmail.com",
-      status: "active",
-      transaction: "Rp.4.500.000",
-    },
-    {
-      id: 4,
-      username: "Alpha",
-      email: "alpha@gmail.com",
-      status: "nonactive",
-      transaction: "Rp.3.500.000",
-    },
-    {
-      id: 5,
-      username: "Rudi",
-      email: "rudi@gmail.com",
-      status: "active",
-      transaction: "Rp.8.000.000",
-    }
-  ];
+export const users = [{
+  "ID": 3,
+  "Name": "Ada gajah",
+  "Email": "alpha@gmail.com",
+  "PhoneNumber": "08342383442",
+  "BirthDate": "1991-01-05"
+}, {
+  "ID": 4,
+  "Name": "Ada gajah",
+  "Email": "alpha@gmail.com",
+  "PhoneNumber": "08342383442",
+  "BirthDate": "1991-01-05"
+}, {
+  "ID": 1,
+  "Name": "Sera",
+  "Email": "ryan@gmail.com",
+  "PhoneNumber": "08342383432",
+  "BirthDate": "1991-01-01"
+}, {
+  "ID": 2,
+  "Name": "Ada anjing",
+  "Email": "andre@gmail.com",
+  "PhoneNumber": "08342383436",
+  "BirthDate": "1991-01-03"
+}, {
+  "ID": 5,
+  "Name": "Ada gajah",
+  "Email": "alpha@gmail.com",
+  "PhoneNumber": "08342383442",
+  "BirthDate": "1991-01-05"
+}];
 
-  export const productRows = [
-    {
-      id: 1,
-      name: "Gitar Fender",
-      img:
-        "fender.jpg",
-      stock: 100,
-      status: "active",
-      price: "Rp.25.050.000",
-    },
-    {
-      id: 2,
-      name: "Keyboard Piano",
-      img:
-        "piano.jpg",
-      stock: 75,
-      status: "active",
-      price: "Rp.10.500.000",
-    },
-    {
-      id: 3,
-      name: "Drums",
-      img:
-        "drum.jpg",
-      stock: 50,
-      status: "active",
-      price: "Rp.8.000.000",
-    },
-    {
-      id: 4,
-      name: "Saxophone",
-      img:
-        "sexo.png",
-      stock: 60,
-      status: "active",
-      price: "Rp.5.500.000",
-    },
-    {
-      id: 5,
-      name: "Speaker Ampli",
-      img:
-        "ampli.jpg",
-      stock: 55,
-      status: "active",
-      price: "Rp.5.550.000",
-    }
-  ];
+export const userRows = [
+  {
+    id: 1,
+    username: "Ryan W",
+    email: "ryan@gmail.com",
+    status: "active",
+    transaction: "Rp.5.050.000",
+  },
+  {
+    id: 2,
+    username: "Andre",
+    email: "andre@gmail.com",
+    status: "active",
+    transaction: "Rp.1.550.000",
+  },
+  {
+    id: 3,
+    username: "Doni",
+    email: "doni@gmail.com",
+    status: "active",
+    transaction: "Rp.4.500.000",
+  },
+  {
+    id: 4,
+    username: "Alpha",
+    email: "alpha@gmail.com",
+    status: "nonactive",
+    transaction: "Rp.3.500.000",
+  },
+  {
+    id: 5,
+    username: "Rudi",
+    email: "rudi@gmail.com",
+    status: "active",
+    transaction: "Rp.8.000.000",
+  }
+];
+
+export const productRows = [
+  {
+    id: 1,
+    name: "Gitar Fender",
+    img:
+      "fender.jpg",
+    stock: 100,
+    status: "active",
+    price: "Rp.25.050.000",
+  },
+  {
+    id: 2,
+    name: "Keyboard Piano",
+    img:
+      "piano.jpg",
+    stock: 75,
+    status: "active",
+    price: "Rp.10.500.000",
+  },
+  {
+    id: 3,
+    name: "Drums",
+    img:
+      "drum.jpg",
+    stock: 50,
+    status: "active",
+    price: "Rp.8.000.000",
+  },
+  {
+    id: 4,
+    name: "Saxophone",
+    img:
+      "sexo.png",
+    stock: 60,
+    status: "active",
+    price: "Rp.5.500.000",
+  },
+  {
+    id: 5,
+    name: "Speaker Ampli",
+    img:
+      "ampli.jpg",
+    stock: 55,
+    status: "active",
+    price: "Rp.5.550.000",
+  }
+];
--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,146 +1,157 @@
-// import { DataGrid } from "@material-ui/data-grid";
-// import { DeleteOutline } from "@material-ui/icons";
-// import { userRows } from "../../dummyData";
-// import { Link } from "react-router-dom";
+import { DataGrid } from "@material-ui/data-grid";
+import { DeleteOutline } from "@material-ui/icons";
+import { userRows, users } from "../../dummyData";
+import { Link } from "react-router-dom";
 // import { useState } from "react";
 import axios from "axios";
 import { useEffect,useState } from "react";
 import "./userList.css";
-import { DataGrid } from '@mui/x-data-grid';
+// import { DataGrid } from '@mui/x-data-grid';
 
-const UserList = () => {
-  const [product,setProduct] = useState([])
-  const [search,setSearch] = useState("")
+// const UserList = () => {
+//   const [product,setProduct] = useState()
+//   const [search,setSearch] = useState("")
+//
 
+//  componentDidMount
+//     useEffect(() => {
+//      console.log('A');
+//       getProductData();
+//     }, [];
+//     return (
+//       <div className="UserList">
+//       <h1>User List</h1>
+//       <input type="text"
+//       placeholder="search here"
+//       onChange={(e) =>{
+//         setSearch(e.target.value);
+//       }}
+//       />
+//
+//       {product.filter((item) => {
+//         if (search=="") {
+//           return item
+//         }
+//         else if (item.name.toLowerCase().includes(search.toLowerCase())){
+//           return item
+//         }
+//       }).
+//       map((item) => {
+//         return (
+//           <p>
+//             {item.name} - {item.price}
+//           </p>
+//         );
+//       })}
+//       </div>
+//   );
+// };
+//
+//
+export default function UserList() {
+
+
+  const [data, setData] = useState(users);
+  // TODO : Use below line and remove above line
+  // const [data, setData] = useState([]);
+
+  // TODO : validate getProductData and useEffect method to reflect current UI if it's displaye the correct data
   const getProductData = async () => {
     try{
-      const data = await axios.get(
-        "http://localhost:8080/user/list")
+      const data = await axios.get("http://localhost:8080/user/list");
       console.log(data);
-      setProduct(data.data)
+      setData(data);
     } catch(e){
       console.log(e)
-      }
-    };
+    }
+  };
 
-    useEffect(() => {
-      getProductData();
-    }, []);
-    return (
-      <div className="UserList">
-      <h1>User List</h1>
-      <input type="text"
-      placeholder="search here"
-      onChange={(e) =>{
-        setSearch(e.target.value);
-      }}
-      />
+  useEffect(() => {
+    getProductData();
+  }, []);
 
-      {product.filter((item) => {
-        if (search=="") {
-          return item
-        }
-        else if (item.name.toLowerCase().includes(search.toLowerCase())){
-          return item
-        }
-      }).
-      map((item) => {
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const columns = [
+    // TODO : Tell the lead to fix the ID naming format
+    { field: "id", headerName: "ID", width: 90 },
+    // TODO : fix the "field" values
+    {
+      field: "Name",
+      headerName: "User",
+      width: 200,
+      renderCell: (params) => {
         return (
-          <p>
-            {item.name} - {item.price}
-          </p>
+
+          <div className="userListUser">
+            <img className="#" src={params.row.avatar} alt="" />
+            {params.row.username}
+          </div>
         );
-      })}
-      </div>
+      },
+    },
+    { field: "email", headerName: "Email", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 120,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 120,
+      renderCell: (params) => {
+        return (
+          <>
+           {/* <Link to={"/newUser/" + params.row.id}>
+              <button className="userListEdit">Create</button>
+            </Link> */}
+            <Link to={"/user/" + params.row.id}>
+              <button className="userListEdit">Edit</button>
+            </Link>
+            <DeleteOutline
+              className="userListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+    <div className="user">
+    <div className="userTitleContainer">
+    <h1 className="userTitle">Create</h1>
+    <Link to="/newUser">
+      <button className="userAddButton">Create</button>
+    </Link>
+  </div>
+  </div>
+    // {
+    //   field: "Create",
+    //   headerName: "Create",
+    //   width: 200 ,
+    //   renderCell: (params) => {
+    //     return (
+
+    //       <Link to={"/newUser/" + params.row.id}>
+    //       <button className="userListEdit">Create</button>
+    //     </Link>
+    //     );
+    //   },
+    // },
+  ];
+
+  return (
+    <div className="userList">
+      <DataGrid
+        rows={data}
+        disableSelectionOnClick
+        columns={columns}
+        pageSize={8}
+        checkboxSelection
+      />
+    </div>
   );
-};
+}
 
-
-export default UserList() 
-
-
-//   const [data, setData] = useState(userRows);
-
-//   const handleDelete = (id) => {
-//     setData(data.filter((item) => item.id !== id));
-//   };
-
-//   const columns = [
-    
-//     { field: "id", headerName: "ID", width: 90 },
-//     {
-//       field: "user",
-//       headerName: "User",
-//       width: 200,
-//       renderCell: (params) => {
-//         return (
-          
-//           <div className="userListUser">
-//             <img className="#" src={params.row.avatar} alt="" />
-//             {params.row.username}
-//           </div>
-//         );
-//       },
-//     },
-//     { field: "email", headerName: "Email", width: 200 },
-//     {
-//       field: "status",
-//       headerName: "Status",
-//       width: 120,
-//     },
-//     {
-//       field: "action",
-//       headerName: "Action",
-//       width: 120,
-//       renderCell: (params) => {
-//         return (
-//           <>
-//            {/* <Link to={"/newUser/" + params.row.id}>
-//               <button className="userListEdit">Create</button>
-//             </Link> */}
-//             <Link to={"/user/" + params.row.id}>
-//               <button className="userListEdit">Edit</button>
-//             </Link>
-//             <DeleteOutline
-//               className="userListDelete"
-//               onClick={() => handleDelete(params.row.id)}
-//             />
-//           </>
-//         );
-//       },
-//     },
-//     <div className="user">
-//     <div className="userTitleContainer">
-//     <h1 className="userTitle">Create</h1>
-//     <Link to="/newUser">
-//       <button className="userAddButton">Create</button>
-//     </Link>
-//   </div>
-//   </div>
-//     // {
-//     //   field: "Create",
-//     //   headerName: "Create",
-//     //   width: 200 ,
-//     //   renderCell: (params) => {
-//     //     return (
-          
-//     //       <Link to={"/newUser/" + params.row.id}>
-//     //       <button className="userListEdit">Create</button>
-//     //     </Link>
-//     //     );
-//     //   },
-//     // },
-//   ];
-
-//   return (
-//     <div className="userList">
-//       <DataGrid
-//         rows={data}
-//         disableSelectionOnClick
-//         columns={columns}
-//         pageSize={8}
-//         checkboxSelection
-//       />
-//     </div>
-//   );
-// }
