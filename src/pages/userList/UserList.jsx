aadb89fc41c0446bf15b142bc91b91f1c9579e50
widhistira <widhistira79Agmail.com>
2021-12-20 16:15:57 +0700

    perubahan struktur

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,66 +1,21 @@
 import { DataGrid } from "@material-ui/data-grid";
 import { DeleteOutline } from "@material-ui/icons";
-import { userRows, users } from "../../dummyData";
+// import { userRows, users } from "../../dummyData";
 import { Link } from "react-router-dom";
-// import { useState } from "react";
 import axios from "axios";
 import { useEffect,useState } from "react";
 import "./userList.css";
 // import { DataGrid } from '@mui/x-data-grid';
 
-// const UserList = () => {
-//   const [product,setProduct] = useState()
-//   const [search,setSearch] = useState("")
-//
-
-//  componentDidMount
-//     useEffect(() => {
-//      console.log('A');
-//       getProductData();
-//     }, [];
-//     return (
-//       <div className="UserList">
-//       <h1>User List</h1>
-//       <input type="text"
-//       placeholder="search here"
-//       onChange={(e) =>{
-//         setSearch(e.target.value);
-//       }}
-//       />
-//
-//       {product.filter((item) => {
-//         if (search=="") {
-//           return item
-//         }
-//         else if (item.name.toLowerCase().includes(search.toLowerCase())){
-//           return item
-//         }
-//       }).
-//       map((item) => {
-//         return (
-//           <p>
-//             {item.name} - {item.price}
-//           </p>
-//         );
-//       })}
-//       </div>
-//   );
-// };
-//
-//
 export default function UserList() {
 
 
-  const [data, setData] = useState(users);
-  // TODO : Use below line and remove above line
-  // const [data, setData] = useState([]);
-
-  // TODO : validate getProductData and useEffect method to reflect current UI if it's displaye the correct data
+  const [data, setData] = useState([]);
   const getProductData = async () => {
     try{
       const data = await axios.get("http://localhost:8080/user/list");
       console.log(data);
-      setData(data);
+      setData(data.data);
     } catch(e){
       console.log(e)
     }
@@ -75,27 +30,32 @@ export default function UserList() {
   };
 
   const columns = [
-    // TODO : Tell the lead to fix the ID naming format
     { field: "id", headerName: "ID", width: 90 },
-    // TODO : fix the "field" values
     {
-      field: "Name",
-      headerName: "User",
+      field: "name",
+      headerName: "Name",
       width: 200,
-      renderCell: (params) => {
-        return (
+      // renderCell: (params) => {
+      //   return (
 
-          <div className="userListUser">
-            <img className="#" src={params.row.avatar} alt="" />
-            {params.row.username}
-          </div>
-        );
-      },
+      //     <div className="userListUser">
+      //       {/* <img className="#" src={params.row.avatar} alt="" />
+      //       {params.row.username} */}
+      //     </div>
+      //   );
+      // },
     },
-    { field: "email", headerName: "Email", width: 200 },
+    { field: "email", 
+      headerName: "Email", 
+      width: 200 },
     {
-      field: "status",
-      headerName: "Status",
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      width: 120,
+    },
+    {
+      field: "birthDate",
+      headerName: "Birthdate",
       width: 120,
     },
     {
@@ -127,19 +87,6 @@ export default function UserList() {
     </Link>
   </div>
   </div>
-    // {
-    //   field: "Create",
-    //   headerName: "Create",
-    //   width: 200 ,
-    //   renderCell: (params) => {
-    //     return (
-
-    //       <Link to={"/newUser/" + params.row.id}>
-    //       <button className="userListEdit">Create</button>
-    //     </Link>
-    //     );
-    //   },
-    // },
   ];
 
   return (
